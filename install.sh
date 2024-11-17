997b0ec74c4ded05fc0aa7c8f319f4c7d259ff5b
Simone Karin Lehmann <skl@lisanet.de>
2024-11-17 13:00:26 +0100

    add install and uninstall scripts

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+sudo cp bisync /usr/local/bin
+sudo chmod a+x /usr/local/bin/bisync
---
+++ uninstall.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+# delete binary
+sudo rm -f /usr/local/bin/bisync
+# delete support dir and files
+sudo rm -rf "$HOME/Library/Application Support/de.lisanet.bisync"
